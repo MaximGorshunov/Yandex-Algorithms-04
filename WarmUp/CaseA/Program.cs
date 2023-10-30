2775073cc41b53e65d68f51bfe06f8d9665c816b
Maxim Gorshunov <maximgorshunov@gmail.com>
2023-10-31 00:24:13 +0300

    Done warm-up case A and case B

---
+++ WarmUp/CaseA/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace CaseA;
+
+internal sealed class Program 
+{
+    static void Main() {
+        
+        var mL = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray();
+        var sequence = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray();
+
+        for (var i = 0; i < mL[1]; i++) 
+        {
+            var lR = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray();
+            var localMin = sequence[lR[0]];
+            bool notFound = true;
+
+            for (var j = lR[0]+1; j <= lR[1]; j++)
+            {
+                if (sequence[j] > localMin) 
+                {
+                    Console.WriteLine(sequence[j]);
+                    notFound = false;
+                    break;
+                }
+
+                if (sequence[j] < localMin) {
+                    Console.WriteLine(localMin);
+                    notFound = false;
+                    break;
+                }
+            }
+
+            if (notFound) Console.WriteLine("NOT FOUND");
+        } 
+    }
+}
---
+++ WarmUp/CaseB/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace CaseB;
+
+internal sealed class Program 
+{
+    static void Main(){
+
+        var input = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray();
+        
+        var lcd = FindLcd(input[1], input[3]);
+        var m = input[0] * lcd/input[1] + input[2] * lcd/input[3];
+        var gcd = FindGcd(m, lcd);
+        
+        Console.WriteLine(m/gcd + " " + lcd/gcd);
+    }
+    
+    private static int FindLcd(int num1, int num2) {
+        
+        int maxNum = Math.Max(num1, num2);
+        int lcd = num1 * num2;
+
+        for (int i = maxNum; i <= lcd; i += maxNum) {
+            if (i % num1 == 0 && i % num2 == 0) {
+                lcd = i;
+                break;
+            }
+        }
+
+        return lcd;
+    }
+    
+    private static int FindGcd(int num1, int num2)
+    {
+        while (num2 != 0)
+        {
+            int temp = num2;
+            num2 = num1 % num2;
+            num1 = temp;
+        }
+        return num1;
+    }
+}
