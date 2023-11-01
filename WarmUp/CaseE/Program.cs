57cc2f5fb4741e9bd291ccc1c0820232c49642d2
Maxim Gorshunov <maximgorshunov@gmail.com>
2023-11-02 00:06:01 +0300

    Done warm-up case E

---
+++ WarmUp/CaseE/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace CaseE;
+
+internal sealed class Program
+{
+    private static void Main()
+    {
+        var students = Convert.ToInt32(Console.ReadLine());
+        var ratings = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+        
+        var prefixSums = new int[students + 1];
+        
+        for (var i = 0; i < students; i++)
+        {
+            prefixSums[i + 1] = prefixSums[i] + ratings[i];
+        }
+        
+        for (var i = 0; i < students; i++)
+        {
+            var result = ratings[i] * (2 * i - students + 2) - 2 * prefixSums[i + 1] + prefixSums[students];
+            Console.WriteLine(result);
+        }
+    }
+}
