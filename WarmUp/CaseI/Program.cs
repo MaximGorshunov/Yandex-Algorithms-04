02e254688032f6a5238bc52d58f86f0fd2a20372
Maxim Gorshunov <maximgorshunov@gmail.com>
2023-11-04 01:10:02 +0300

    Done warm-up case I

---
+++ WarmUp/CaseI/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace CaseI;
+
+internal sealed class Program
+{
+    private static void Main()
+    {
+        var input = Console.ReadLine();
+        
+        Console.WriteLine(CheckBraces(input) ? "yes" : "no");
+    }
+
+    private static bool CheckBraces(string input)
+    {
+        var stack = new Stack<char>();
+        
+        foreach (var chr in input)
+        {
+            switch (chr)
+            {
+                case ')' :
+                    if (stack.Count == 0 || stack.Peek() != '(') return false;
+                    stack.Pop();
+                    break;
+                case '}' :
+                    if (stack.Count == 0 || stack.Peek() != '{') return false;
+                    stack.Pop();
+                    break;
+                case ']' :
+                    if (stack.Count == 0 || stack.Peek() != '[') return false;
+                    stack.Pop();
+                    break;
+                default:
+                    stack.Push(chr);
+                    break;
+            }
+        }
+
+        return stack.Count == 0;
+    }
+}
