db80fc19573a82e08b570e653ac9e858e85afc1b
Maxim Gorshunov <maximgorshunov@gmail.com>
2023-11-03 20:14:49 +0300

    Done warm-up case H

---
+++ WarmUp/CaseH/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace CaseH;
+
+class Program
+{
+    static void Main()
+    {
+        int a, b, n;
+        a = int.Parse(Console.ReadLine());
+        b = int.Parse(Console.ReadLine());
+        n = int.Parse(Console.ReadLine());
+
+        if (a > Math.Ceiling((double)b / n))
+        {
+            Console.WriteLine("Yes");
+        }
+        else
+        {
+            Console.WriteLine("No");
+        }
+    }
+}
